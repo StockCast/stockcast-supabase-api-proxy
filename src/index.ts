ee1d975126ee6a74f4961118a22b4068b962b6a7
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2026-03-27 15:34:15 -0300

    fix: do not redirect internally

--- src/index.ts
+++ src/index.ts
@@ -27,28 +27,13 @@ export default {
       method: request.method,
       headers,
       body: request.body,
+      redirect: 'manual', // prevent Worker to follow Google link
     })
 
     const responseHeaders = new Headers(response.headers)
     for (const [key, value] of Object.entries(CORS_HEADERS)) {
       responseHeaders.set(key, value)
     }
-
-    responseHeaders.delete('Content-Security-Policy');
-
-	const myCSP = [
-		"default-src 'self' https://*.stockcast.com.br",
-		"script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: data: https://apis.google.com https://accounts.google.com https://www.gstatic.com https://ssl.gstatic.com https://www.google.com https://static.cloudflareinsights.com https://*.google-analytics.com",
-		"connect-src 'self' https://*.stockcast.com.br https://*.supabase.co wss://*.supabase.co https://accounts.google.com https://*.sentry.io https://*.google-analytics.com",
-		"frame-src 'self' https://accounts.google.com https://*.supabase.co https://*.stockcast.com.br",
-		"base-uri 'self' https://accounts.google.com",
-		"img-src 'self' data: https://*.googleusercontent.com https://www.gstatic.com https://www.google.com",
-		"style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.gstatic.com",
-		"font-src 'self' https://fonts.gstatic.com"
-	].join('; ');
-    responseHeaders.set('Content-Security-Policy', myCSP);
-		responseHeaders.set('Cross-Origin-Resource-Policy', 'cross-origin');
-		responseHeaders.set('Cross-Origin-Allow-Origin', '*');
 
     return new Response(response.body, {
       status: response.status,

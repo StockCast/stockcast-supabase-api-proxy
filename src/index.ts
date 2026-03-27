0575379f09cc678072b24084129d5a2c07da390c
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2026-03-27 12:38:01 -0300

    feat: add additional headers to avoid CORS problems

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ interface Env {
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-client-info, apikey, Prefer, Range, If-None-Match',
   'Access-Control-Max-Age': '86400',
 }
 
